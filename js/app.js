a84e4c9ba14c607386e625af51e1ea058caffd4d
grantb4812 <grantb4812@gmail.com>
2015-08-14 18:09:00 -0500

    fixed the bug

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
+//need to figure out off
+
+
 $(document).ready(function(){
 	$('.ryu').mouseenter(function(){
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
+		ryuReady();
 	})
+		
+		//$("window").off("keydown", 'window', showCool);
 	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
+			ryuStill();
+		//$('window').on("keydown", 'window', showCool);
 	});
 
 	$('.ryu').mousedown(function(){
 		playHadouken();
+		$('.ryu-still').hide();
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
 			{'left': '1020px'},
@@ -22,21 +28,18 @@ $(document).ready(function(){
 		);
 	})
 	.mouseup(function(){
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
+		ryuReady();
 
 	});
 
 	
-		$(window).keydown(function(event){
-			if (event.keyCode == 88){
-				$('.ryu-still').hide();
-				$('.ryu-cool').show();
-			}
-		})
-		.keyup(function(){
-			$('.ryu-cool').hide();
-			$('.ryu-still').show();
+		$(window).on("keydown", function(event){
+				ryuCool();	
+		}).on('keyup', function(event){
+				ryuStill();
+				if($('.ryu').mouseenter()) {
+					ryuReady();
+				}
 		});
 
 });
@@ -46,3 +49,43 @@ function playHadouken () {
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 };
+
+function showCool (event) {
+		if (event.keyCode == 88){
+				$('.ryu-still').hide();
+				$('.ryu-ready').hide();
+				$('.ryu-throwing').hide();
+				$('.ryu-cool').show();
+			}
+};
+
+function showStill() {
+			$('.ryu-cool').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-still').show();
+};
+
+function ryuReady() {
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide()
+		$('.ryu-ready').show();
+};
+
+function ryuStill () {
+		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide()
+		$('.ryu-still').show();
+};
+
+function ryuCool () {
+	if (event.keyCode == 88){
+				$('.ryu-still').hide();
+				$('.ryu-ready').hide();
+				$('.ryu-throwing').hide();
+				$('.ryu-cool').show();
+	}
+};
+
